6397d614e5f7717d449bf68e48f450b609f6bee1
floatingpurr <floatingpurr@users.noreply.github.com>
2020-12-09 23:06:10 +0100

    Change naming convention

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ from datetime import date
 from openpyxl import load_workbook
 import csv
 
-data_path = 'data/staging'
+data_path = 'data/latest'
 today = date.today()
 current_date = today.strftime('%d-%m-%Y')
 
@@ -10,7 +10,7 @@ wb = load_workbook(f'{data_path}/data.xlsx', read_only=True)
 
 
 for sheet in wb.worksheets:
-    with open(f'{data_path}/{current_date}-{sheet.title}.csv', 'w', newline="") as f:
+    with open(f'{data_path}/{sheet.title}.csv', 'w', newline="") as f:
         c = csv.writer(f)
         for r in sheet.rows:
             c.writerow([cell.value for cell in r])
