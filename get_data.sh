50064fd83b1c57532ff71bff1a66e8379376f934
floatingpurr <floatingpurr@users.noreply.github.com>
2020-12-09 17:43:19 +0100

    Scaffolding

---
+++ get_data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Get and save latest data
+curl https://www.epicentro.iss.it/coronavirus/open-data/covid_19-iss.xlsx --output data/staging/data.xlsx
---
+++ parser.py
@@ -0,0 +1,16 @@
+from datetime import date
+from openpyxl import load_workbook
+import csv
+
+data_path = 'data/staging'
+today = date.today()
+current_date = today.strftime('%d-%m-%Y')
+
+wb = load_workbook(f'{data_path}/data.xlsx', read_only=True)
+
+
+for sheet in wb.worksheets:
+    with open(f'{data_path}/{current_date}-{sheet.title}.csv', 'w', newline="") as f:
+        c = csv.writer(f)
+        for r in sheet.rows:
+            c.writerow([cell.value for cell in r])
