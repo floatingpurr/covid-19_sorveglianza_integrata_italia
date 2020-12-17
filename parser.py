9286a1ee81180fab9b2632bdaba6d5cc639e7c45
floatingpurr <floatingpurr@users.noreply.github.com>
2020-12-17 12:17:48 +0100

    Fix the workflow. Fix #1

--- parser.py
+++ parser.py
@@ -1,14 +1,17 @@
-from datetime import date
 from openpyxl import load_workbook
 import csv
 
 data_path = 'data/latest'
-today = date.today()
-current_date = today.strftime('%d-%m-%Y')
 
 wb = load_workbook(f'{data_path}/data.xlsx', read_only=True)
+# get report date
+report_date = wb['casi_regioni']['A2'].value.replace('/','-')
 
+# Write date to file
+with open(f'{data_path}/date', 'w') as f:
+    f.write(report_date)
 
+# Write data to file
 for sheet in wb.worksheets:
     with open(f'{data_path}/{sheet.title}.csv', 'w', newline="") as f:
         c = csv.writer(f)
