ff116b0ef3c8b0bec25d58c43d50bd723a8afaae
Andrea <floatingpurr@users.noreply.github.com>
2021-03-18 09:48:05 +0100

    Fix SSL issues

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Get and save latest data
-curl https://www.epicentro.iss.it/coronavirus/open-data/covid_19-iss.xlsx --output data/latest/data.xlsx
+curl -k --ciphers 'DEFAULT:!DH' https://www.epicentro.iss.it/coronavirus/open-data/covid_19-iss.xlsx --output data/latest/data.xlsx 
